75b8e1b37c975045b9d6d485fd5c8ad90942a282
lsw1104 <tomy807@naver.com>
2022-03-20 18:50:14 +0900

    왼쪽 상단 전체메뉴 -중고거래:Category -생활: Life

---
+++ src/main/java/com/example/demo/src/category/CategoryController.java
@@ -0,0 +1,32 @@
+package com.example.demo.src.category;
+
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.category.model.GetMenuRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryProvider categoryProvider;
+
+    @GetMapping("")
+    public BaseResponse<List<GetMenuRes>> getMenu() {
+
+        try {
+            List<GetMenuRes> menu = categoryProvider.getMenu();
+            return new BaseResponse<>(menu);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+}
