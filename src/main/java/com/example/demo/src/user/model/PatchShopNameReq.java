57b0c37eaf4bf66acfc897dfdda5dcf4db4b2725
lsw1104 <tomy807@naver.com>
2022-03-22 22:38:30 +0900

    변수 1개면 오류?

--- src/main/java/com/example/demo/src/user/model/PatchShopNameReq.java
+++ src/main/java/com/example/demo/src/user/model/PatchShopNameReq.java
@@ -1,10 +1,13 @@
 package com.example.demo.src.user.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class PatchShopNameReq {
+    private String userIdx;
     private String shopName;
 }
