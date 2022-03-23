b96004650f227a624581ebf21a0e12e25cd7b92b
lsw1104 <tomy807@naver.com>
2022-03-23 19:51:15 +0900

    최근 본 상품 조회 완성

--- src/main/java/com/example/demo/src/recent/RecentDao.java
+++ src/main/java/com/example/demo/src/recent/RecentDao.java
@@ -1,8 +1,7 @@
-package com.example.demo.src.favorite;
-
+package com.example.demo.src.recent;
 
 import com.example.demo.src.favorite.model.GetFavoriteRes;
-import com.example.demo.src.product.model.GetProductRes;
+import com.example.demo.src.recent.model.GetRecentRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,7 @@ import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
-public class FavoriteDao {
+public class RecentDao {
 
     private JdbcTemplate jdbcTemplate;
 
@@ -20,33 +19,7 @@ public class FavoriteDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void createFavorite(int productIdx, int userIdx) {
-        jdbcTemplate.update("insert into Favorites(user_id, product_id) VALUE (?,?)", userIdx, productIdx);
-    }
-
-
-
-    public int checkIsFavorite(int productIdx, int userIdx) {
-        String checkIsFavoriteQuery = "select exists(select * from Favorites where product_id = ? and user_id = ? and status='SAVED')";
-        return this.jdbcTemplate.queryForObject(checkIsFavoriteQuery, int.class, productIdx, userIdx);
-    }
-
-    public void deleteFavorite(int productIdx, int userIdx) {
-        Object[] deleteFavoriteParams = new Object[]{productIdx, userIdx};
-        jdbcTemplate.update("update Favorites set status='DELETED' where product_id=? and user_id=?", deleteFavoriteParams);
-    }
-
-    public int checkDeletedFavorite(int productIdx, int userIdx) {
-        String checkIsFavoriteQuery = "select exists(select * from Favorites where product_id = ? and user_id = ? and status='DELETED')";
-        return this.jdbcTemplate.queryForObject(checkIsFavoriteQuery, int.class, productIdx, userIdx);
-
-    }
-
-    public void recreateFavorite(int productIdx, int userIdx) {
-        jdbcTemplate.update("update Favorites set status='SAVED' where product_id=? and user_id=?", userIdx, productIdx);
-    }
-
-    public List<GetFavoriteRes> getFavorites(int userIdx) {
+    public List<GetRecentRes> getRecents(int userIdx) {
         String getFavoritesQuery = "select P.product_id as productIdx,\n" +
                 "       (select product_image_url from ProductImages where ProductImages.product_id = P.product_id limit 1) as productImg,\n" +
                 "       P.product_title as title,\n" +
@@ -55,7 +28,6 @@ public class FavoriteDao {
                 "            when sell_status = 'RESERVED' then '예약중'\n" +
                 "            when sell_status = 'SOLDOUT' then '판매완료'\n" +
                 "        end as sellStatus,\n" +
-                "       U.shop_name as shopName,\n" +
                 "       P.secure_payment as securePayment,\n" +
                 "       (case when timestampdiff(second , P.createdAt, current_timestamp) <60\n" +
                 "                then concat(timestampdiff(second, P.createdAt, current_timestamp),' 초 전')\n" +
@@ -65,20 +37,19 @@ public class FavoriteDao {
                 "                then concat(timestampdiff(hour, P.createdAt, current_timestamp),' 시간 전')\n" +
                 "            else concat(datediff( current_timestamp, P.createdAt),' 일 전')\n" +
                 "        end) as createdAt\n" +
-                "from Favorites\n" +
-                "         inner join Products P on Favorites.product_id = P.product_id\n" +
-                "         inner join Users U on P.user_id = U.user_id\n" +
-                "where Favorites.user_id=?";
+                "from RecentProducts\n" +
+                "         inner join Products P on RecentProducts.product_id = P.product_id\n" +
+                "where RecentProducts.user_id=?";
         return this.jdbcTemplate.query(getFavoritesQuery,
-                (rs, rowNum) -> new GetFavoriteRes(
+                (rs, rowNum) -> new GetRecentRes(
                         rs.getInt("productIdx"),
                         rs.getString("productImg"),
                         rs.getString("title"),
                         rs.getInt("price"),
                         rs.getString("sellStatus"),
-                        rs.getString("shopName"),
                         rs.getString("securePayment"),
                         rs.getString("createdAt")
                 ), userIdx);
+
     }
 }
--- src/main/java/com/example/demo/src/recent/model/GetRecentRes.java
+++ src/main/java/com/example/demo/src/recent/model/GetRecentRes.java
@@ -1,4 +1,4 @@
-package com.example.demo.src.favorite.model;
+package com.example.demo.src.recent.model;
 
 
 import lombok.AllArgsConstructor;
@@ -8,15 +8,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class GetFavoriteRes {
-
+public class GetRecentRes {
     private Integer productIdx;
     private String productImg;
     private String title;
     private Integer price;
     private String sellStatus;
-    private String shopName;
     private String securePayment;
     private String createdAt;
-
 }
