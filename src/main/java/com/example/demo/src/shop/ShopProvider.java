c0cee5f1d9b93e117ae8f700539706209447bd55
lsw1104 <tomy807@naver.com>
2022-03-24 15:03:03 +0900

    사용자 정보 조회 완성

--- src/main/java/com/example/demo/src/shop/ShopProvider.java
+++ src/main/java/com/example/demo/src/shop/ShopProvider.java
@@ -1,4 +1,11 @@
 package com.example.demo.src.shop;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.src.shop.model.GetShopRes;
+
 public class ShopProvider {
+    public GetShopRes getShop(int userIdx) throws BaseException {
+
+        return null;
+    }
 }
